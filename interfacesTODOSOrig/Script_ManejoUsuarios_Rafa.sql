c6fba497a3e1d3ed5204d08f74295e1697cfe416
jmorenor.1tic@gmail.com <jmorenor.1tic@gmail.com>
2019-01-14 09:18:17 +0100

    Añadido tabla producto y cambio del nombre del id del producto del archivo.sql

--- interfacesTODOSOrig/Script_ManejoUsuarios_Rafa.sql
+++ interfacesTODOSOrig/Script_ManejoUsuarios_Rafa.sql
@@ -19,13 +19,22 @@ create user miguel identified by'12345';
 grant select on usuarios.* to miguel;
 -- grant all privileges on usuarios.* to miguel;
 
+-- crear tabla producto
+create table producto(
+	idProducto int(5) primary key,
+	nombre varchar(40),
+    precio double
+);
+
+insert into producto values(1,'jamon',12);
+
 -- crear tabla compras(solo empresa)
 create table compras(
 	id int(5),
 	usuario varchar(40),
     producto int(5),
     fecha timestamp,
-	FOREIGN KEY (producto) REFERENCES productos(id),
+	FOREIGN KEY (producto) REFERENCES producto(idProducto),
     FOREIGN KEY (usuario) REFERENCES usuario(email)
 );
 -- crear ventana compra (solo si es uan empresa)
